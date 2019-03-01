e91173c427d296db64fb771448bfd1691721762b
Huy Vuong <vuongnguyenhuy@yahoo.com>
2019-03-01 00:43:20 -0600

    realloc implementation finished, implicit free implemented

--- mm.c
+++ mm.c
@@ -82,8 +82,8 @@ team_t team = {
 #define FTRP(bp)	((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
 
 /* A good way to access block predecessor and successor */
-#define PRED_PTR(bp)	(*(void **)(bp))
-#define SUCC_PTR(bp)	(*(void **)(bp + WSIZE))
+#define PRED_PTR(bp)	(*(char **)(bp))
+#define SUCC_PTR(bp)	(*(char **)(bp + WSIZE))
 // #define PRED(bp)	(*(char **)(bp))
 // #define SUCC(bp)	(*(char **)(SUCC_PTR(bp)))
 
@@ -91,6 +91,12 @@ team_t team = {
 /* Given block ptr bp, compute address of next block */
 #define NEXT_BLKP(bp)	((char *)(bp) + GET_SIZE(((char*)(bp) - WSIZE)))
 #define PREV_BLKP(bp)	((char *)(bp) - GET_SIZE(((char*)(bp) - DSIZE)))
+
+
+/* Get and set the first node in a free list of list_num */
+#define GET_ROOT(heap, list)              ((char *)GET((heap) + ((list) * WSIZE)))
+#define SET_ROOT(heap, list, new_root)    (PUT((heap) + ((list) * WSIZE), (new_root)))
+
 /* $end mallocmacros */
 
 
@@ -191,13 +197,13 @@ void *mm_malloc(size_t size)
 // 	else return 9;
 // }
 
-/* Support function to delete a node in a segregrated list */
+/* Support function to delete a block in a segregrated free list */
 static void delete_block(void *bp)
 {
-	printf("Deletting block\n");
-	printf("%p\n", bp);
-	printf("%p\n", PRED_PTR(bp));
-	printf("%p\n", free_listp);
+	// printf("Deletting block\n");
+	// printf("%p\n", bp);
+	// // printf("%p\n", SUCC_PTR(PRED_PTR(bp)));
+	// printf("%p\n", SUCC_PTR(bp));
 	/* if the previous block is free. Set the pointer of the previous block to the next block */
 	if (PRED_PTR(bp))
 	{
@@ -208,7 +214,7 @@ static void delete_block(void *bp)
 	{
 		free_listp = SUCC_PTR(bp);
 	}
-	printf("%p\n", free_listp);
+	// printf("%p\n", free_listp);
 	PRED_PTR(SUCC_PTR(bp)) = PRED_PTR(bp);
 }
 
@@ -222,42 +228,18 @@ void mm_free(void *bp)
 
 	size_t size = GET_SIZE(HDRP(bp));
 	// if (heap_listp == 0) mm_init();
-	PUT(HDRP(bp), PACK(size, 0)); // free block header
-	PUT(FTRP(bp), PACK(size, 0)); // free block footer, avoid internal fragmentation
+	PUT(HDRP(bp), PACK(size, 1)); // free block header
+	PUT(FTRP(bp), PACK(size, 1)); // free block footer, avoid internal fragmentation
 	// Immediate coalescing : coelesce each time free is called
-	char *prev_blk = PREV_BLKP(bp);
-	char *next_blk = NEXT_BLKP(bp);
-	size_t prev_alloc = GET_ALLOC(FTRP(prev_blk)) || PREV_BLKP(bp) == bp;
-	size_t next_alloc = GET_ALLOC(HDRP(next_blk));
-	/* Case 1 : free block in between 2 allocated block */
-	// printf("got here\n");
-	if (prev_alloc && next_alloc)
-	{
-		return;
-
-	}
-	/* Case 2 : free block in between allocated at front and freed block at end */
-	else if (prev_alloc && !next_alloc)
-	{
-
-		size += GET_SIZE(HDRP(next_blk));
-		// /* Delete blocks from the segregated list */
-		// printf("got here\n");
-		delete_block(next_blk);
-		// PUT(HDRP(bp), PACK(size, 0));
-		// PUT(FTRP(bp), PACK(size, 0));
-
-	}
-	/* Case 3 : head alloc is freed and foot alloc is allocated */
-	else if (!prev_alloc && next_alloc)
-	{
-
-	}
-	/* Case 4 : both end are freed */
-	else
-	{
-
-	}
+	// char *prev_blk = PREV_BLKP(bp);
+	// char *next_blk = NEXT_BLKP(bp);
+	// // printf("NEXT_BLKP(bp) : %p  ---- next_blk : %p\n", next_blk, NEXT_BLKP(bp));
+	// size_t prev_alloc = GET_ALLOC(FTRP(prev_blk)) || PREV_BLKP(bp) == bp;
+	// size_t next_alloc = GET_ALLOC(HDRP(next_blk));
+	// if (next_alloc) {
+	// 	size += GET_SIZE(HDRP(next_blk));
+	// 	PUT(HDRP(bp), PACK(size, 1));
+	// }
 
 	coalesce(bp, size);
 
@@ -265,6 +247,64 @@ void mm_free(void *bp)
 
 static void *coalesce(void *bp, size_t size)
 {
+	// Immediate coalescing : coelesce each time free is called
+	char *prev_blk = PREV_BLKP(bp);
+	char *next_blk = NEXT_BLKP(bp);
+	// printf("NEXT_BLKP(bp) : %p  ---- next_blk : %p\n", next_blk, NEXT_BLKP(bp));
+	size_t prev_alloc = GET_ALLOC(FTRP(prev_blk)) || PREV_BLKP(bp) == bp;
+	size_t next_alloc = GET_ALLOC(HDRP(next_blk));
+	if (prev_alloc && next_alloc)
+	{
+		size += GET_SIZE(HDRP(next_blk));
+		// /* Delete blocks from the segregated list */
+		// printf("got here\n");
+		// delete_block(next_blk);
+		PUT(HDRP(bp), PACK(size, 1));
+		PUT(FTRP(bp), PACK(size, 1));
+	}
+	/* Case 2 : free block in between allocated at front and freed block at end */
+	else if (prev_alloc && !next_alloc)
+	{
+
+
+		// PUT(FTRP(bp), PACK(size, 0));
+
+	}
+	/* Case 3 : head alloc is freed and foot alloc is allocated */
+	else if (!prev_alloc && next_alloc)
+	{
+		size += GET_SIZE(HDRP(next_blk));
+		// /* Delete blocks from the segregated list */
+		// printf("got here\n");
+		// delete_block(next_blk);
+		PUT(HDRP(bp), PACK(size, 1));
+		PUT(FTRP(bp), PACK(size, 1));
+	}
+	/* Case 4 : both end are freed */
+	else
+	{
+
+	}
+	int i = 0;
+	char* hp = heap_listp;
+	while(((int)NEXT_BLKP(hp) <(int)bp) && ((int)hp<(int)mem_heap_hi)){
+	//	printf("%d\n", i);
+	//	printf("no\n");
+		hp = NEXT_BLKP(hp);
+		i++;
+	}
+	//	printf("yes\n");
+	//	printf("%p  %p\n",hp, bp);
+	//	printf("%p  %p\n",NEXT_BLKP(hp),bp);
+        if(GET_ALLOC(HDRP(hp)) == 1) {
+                //bp = NEXT_BLKP(bp);
+		PUT(HDRP(hp),PACK(size+=GET_SIZE(HDRP(hp)),1));
+        //      printf("front got coalsed\n");
+		//PUT(HDRP(bp),PACK((size+=GET_SIZE(HDRP(NEXT_BLKP(bp)))),1));
+
+        } else {
+		//printf("didnt do it\n");
+	}
 	return bp;
 }
 
@@ -286,33 +326,66 @@ void *mm_realloc(void *ptr, size_t size)
 	// If the newly allocated size is 0, then it is a free
 	if (size <= 0)
 	{
-		free(ptr);
+		mm_free(ptr);
 	}
 
 	// define new pointer to return
 	size_t old_size = GET_SIZE(HDRP(ptr));
-	char *new_bp;
+	char * new_bp = mm_malloc(size);
 
 	size_t asize; /* Adjusted block size */
-//	size_t extendsize; /* amount to extend heap if no fit */
-	/* Adjust block size to include overhead and alignment reqs. */
-	asize = (size <= WSIZE) ? (WSIZE + OVERHEAD) : (DSIZE + ((size + (OVERHEAD) + (DSIZE - 1)) / DSIZE));
+	//	size_t extendsize; /* amount to extend heap if no fit */
+	if (size <= WSIZE)
+		asize = WSIZE + OVERHEAD;
+    else
+		asize = DSIZE * ((size + (OVERHEAD) + (DSIZE-1)) / DSIZE);
+	// asize = (size <= WSIZE) ? (WSIZE + OVERHEAD) : (DSIZE + ((size + (OVERHEAD) + (DSIZE - 1)) / DSIZE));
 
 	/* new size need more blocks */
-	if (asize > old_size)
+	if (asize <= old_size)
 	{
 //		extendsize = MAX(asize, CHUNKSIZE);
-
+		/* if newly allocated space is smaller than old space, just use the old space */
+		mm_free(new_bp);
+		place(ptr, asize);
+		return ptr;
 	}
 	else
 	{
-		/* if newly allocated space is smaller than old space, just use the old space */
-		place(ptr, asize);
-		// return old pointer since we don't need to rellocate it.
-		return ptr;
+		// mm_free(new_bp);
+		// // mm_free(ptr);
+		// size_t adj_old_size;
+		// if (old_size <= WSIZE)
+		// 	adj_old_size = WSIZE + OVERHEAD;
+	    // else
+		// 	adj_old_size = DSIZE * ((old_size + (OVERHEAD) + (DSIZE-1)) / DSIZE);
+		// /* if the newly allocated place is not enough, then we allocate to a new block pointers
+		// 	then free the old pointers
+		// 	*/
+		// /* Search the free list for a fit */
+	    // if ((new_bp = find_fit(adj_old_size)) != NULL)
+		// {
+		// 	memcpy(new_bp, ptr, adj_old_size);
+		// 	mm_free(ptr);
+		// 	return new_bp;
+		// }
+		// /* No fit found. Get more memory and place the block */
+	    // size_t extendsize = MAX(adj_old_size,CHUNKSIZE);
+		//  //printf("extendsize = %d\n", extendsize);
+	    // if ((new_bp = extend_heap(extendsize/WSIZE)) == NULL)
+	    // {
+		//  	printf("mm_realloc = NULL\n");
+		// 	return NULL;
+	    // }
+		//
+		// memcpy(new_bp, ptr, adj_old_size);
+		// mm_free(ptr);
+		memcpy(new_bp, ptr, old_size);
+		mm_free(ptr);
+		return new_bp;
 	}
-	new_bp = mm_malloc(size);
-	free(ptr);
+
+	// mm_free(ptr);
 	/* You need to implement this function. */
 	return new_bp;
 }
